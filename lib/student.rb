1fccb8446d1bb44153d7dd289328cc3fdfbc7434
Shamel Lakin <christlife4u@gmail.com>
2019-10-21 21:42:53 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -16,17 +16,18 @@ class Student
   end
 
   def add_student_attributes(attributes_hash)
-    self.send("twitter=", attributes_hash[:twitter])
-    self.send("linkedin=", attributes_hash[:linkedin])
-    self.send("github=", attributes_hash[:github])
-    self.send("blog=", attributes_hash[:blog])
-    self.send("profile_quote=", attributes_hash[:profile_quote])
-    self.send("bio=", attributes_hash[:bio])
+    # self.send("twitter=", attributes_hash[:twitter])
+    # self.send("linkedin=", attributes_hash[:linkedin])
+    # self.send("github=", attributes_hash[:github])
+    # self.send("blog=", attributes_hash[:blog])
+    # self.send("profile_quote=", attributes_hash[:profile_quote])
+    # self.send("bio=", attributes_hash[:bio])
+   attributes_hash.each{|key, value| self.send(("#{key}="), value)}
 
   end
 
   def self.all
-    
+    @@all 
   end
 end
 
