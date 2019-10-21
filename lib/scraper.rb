2b57d2a1a16f11eba9111287d770dcb6505fba65
Shamel Lakin <christlife4u@gmail.com>
2019-10-21 20:58:58 +0000

    Done.

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,8 +1,8 @@
 require 'open-uri'
 require 'pry'
-​
+
 class Scraper
-​
+
   def self.scrape_index_page(index_url)
     doc = Nokogiri::HTML(open(index_url))
     student_cards = doc.css('div.roster-cards-container .student-card')
